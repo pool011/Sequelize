a0ad393303cce6ba4d63aba589604407a0055677
Jonah Pool <jpool.school@gmail.com>
2021-04-09 18:22:01 -0400

    Move getFavorites

--- public/script.js
+++ public/script.js
@@ -1,50 +1,7 @@
 // import {mdiStar, mdiStarOutline, mdiStarPlusOutline} from '@mdi/js';
-
-function setFavorites(favorites) {
-  let result;
-  if (favorites === null) {
-    result = ['empty'];
-  } else {
-    result = favorites;
-  } 
-  console.log(typeof result);
-  localStorage.setItem('diningFavorites', JSON.stringify(result));
-}
-
 function getFavorites() {
   const favorites = localStorage.getItem('diningFavorites') === null ? ['empty'] : localStorage.getItem('diningFavorites');
   return JSON.parse(`{"diningFavorites": ${favorites}}`);
-}
-
-function showFavorite(id, isFavorite) {
-  const favoriteButtonDatum = document.createElement('td');
-  const favoriteButton = document.createElement('a');
-  favoriteButton.classList.add('button');
-  favoriteButton.classList.add('favorite');
-  favoriteButton.classList.add('is-inverted');
-  const icon = document.createElement('span');
-  icon.classList.add('icon');
-  const iconImg = document.createElement('i');
-  iconImg.classList.add('fa-star');
-  
-  
-  if (isFavorite) {
-    // favoriteIconRow.innerHTML = '<button class="button favorite" type="button"><span class="icon"><i class="fas fa-star" aria-hidden="false"></i></span></button>';
-    iconImg.classList.add('fas');
-  } else {
-    // favoriteIconRow.innerHTML = '<button class="button favorite" type="button"><span class="icon"><i class="far fa-star" aria-hidden="false"></i></span></button>';
-    iconImg.classList.add('far');
-  }
-
-  iconImg.id = id;
-
-  icon.appendChild(iconImg);
-  favoriteButton.appendChild(icon);
-  favoriteButtonDatum.appendChild(favoriteButton);
-  // const favoriteIcon = favoriteIconElement.querySelector('i');
-  // favoriteIcon.id = id;
-  // console.log('showFavorite:', favoriteIcon.id, favoriteIcon.classList.contains('fas'));
-  return favoriteButtonDatum;
 }
 
 async function getDiningHallData() {
@@ -55,10 +12,58 @@ async function getDiningHallData() {
   return allDiningHalls;
 }
 
+// function removeFavorite(items) {
+//   const favorites = getFavorites();
+//   const {diningFavorites} = favorites;
+//   console.log('items.id', typeof parseInt(items.id,10), 'diningFavorites', typeof diningFavorites[1]);
+//   return parseInt(items.id,10) !== diningFavorites;
+// }
+
 async function populateDiningHalls(halls) {
+  function setFavorites(favorites) {
+    let result;
+    if (favorites === null) {
+      result = ['empty'];
+    } else {
+      result = favorites;
+    } 
+    console.log(typeof result);
+    localStorage.setItem('diningFavorites', JSON.stringify(result));
+  }
+
+  function showFavorite(id, isFavorite) {
+    const favoriteButtonDatum = document.createElement('td');
+    const favoriteButton = document.createElement('a');
+    favoriteButton.classList.add('button');
+    favoriteButton.classList.add('favorite');
+    favoriteButton.classList.add('is-inverted');
+    const icon = document.createElement('span');
+    icon.classList.add('icon');
+    const iconImg = document.createElement('i');
+    iconImg.classList.add('fa-star');
+
+    if (isFavorite) {
+      // favoriteIconRow.innerHTML = '<button class="button favorite" type="button"><span class="icon"><i class="fas fa-star" aria-hidden="false"></i></span></button>';
+      iconImg.classList.add('fas');
+    } else {
+      // favoriteIconRow.innerHTML = '<button class="button favorite" type="button"><span class="icon"><i class="far fa-star" aria-hidden="false"></i></span></button>';
+      iconImg.classList.add('far');
+    }
+ 
+    iconImg.id = id;
+
+    icon.appendChild(iconImg);
+    favoriteButton.appendChild(icon);
+    favoriteButtonDatum.appendChild(favoriteButton);
+    // const favoriteIcon = favoriteIconElement.querySelector('i');
+    // favoriteIcon.id = id;
+    // console.log('showFavorite:', favoriteIcon.id, favoriteIcon.classList.contains('fas'));
+    return favoriteButtonDatum;
+  }
+
   console.log(halls);
   const favorites = getFavorites();
-  const {diningFavorites} = favorites
+  const {diningFavorites} = favorites;
   console.log('favorites', favorites);
   const tableBody = document.querySelector('#hall-list tbody');
   tableBody.innerHTML = '';
@@ -73,57 +78,40 @@ async function populateDiningHalls(halls) {
     newRow.innerHTML = `${showFavorite(id, isFavorite).outerHTML}<th>${hall.hall_id}</th><td>${hall.hall_name}</td><td>${hall.hall_address}</td>`;
     tableBody.append(newRow);
   });
-}
 
-// function removeFavorite(items) {
-//   const favorites = getFavorites();
-//   const {diningFavorites} = favorites;
-//   console.log('items.id', typeof parseInt(items.id,10), 'diningFavorites', typeof diningFavorites[1]);
-//   return parseInt(items.id,10) !== diningFavorites;
-// }
-
-async function favoriteEventHandler(event) {
-  const favorites = getFavorites();
-  const allDiningHalls = await getDiningHallData();
-  const { diningFavorites } = favorites;
-  console.log('Button event fired.');
-  const icon = event.target.querySelector('.fa-star') === null ? event.target : event.target.querySelector('.fa-star');
-  // icon.classList.toggle('fas');
-  // icon.classList.toggle('far');
-  // console.log('items.id', typeof parseInt(icon.id, 10), 'diningFavorites', typeof diningFavorites[1]);
-  if (diningFavorites.includes(parseInt(icon.id, 10))) {
-    console.log('Remove favorite');
-    const newFavorites = diningFavorites.filter((item) => item !== parseInt(icon.id, 10));
-    console.log('newFavorites', newFavorites);
-    setFavorites(newFavorites);
-  } else {
-    console.log('Add favorite');
-    diningFavorites.push(parseInt(icon.id, 10));
-    setFavorites(diningFavorites);
-  }
-  
-  populateDiningHalls(allDiningHalls);
-}
-
-async function favoritesHandler(buttons) {
+  const buttons = document.querySelectorAll('.favorite');
   buttons.forEach((button) => {
-    button.addEventListener('click', favoriteEventHandler);
+    button.addEventListener('click', async (event) => {
+      const allDiningHalls = await getDiningHallData();
+      console.log('Button event fired.');
+      const icon = event.target.querySelector('.fa-star') === null ? event.target : event.target.querySelector('.fa-star');
+      // icon.classList.toggle('fas');
+      // icon.classList.toggle('far');
+      // console.log('items.id', typeof parseInt(icon.id, 10), 'diningFavorites', typeof diningFavorites[1]);
+      if (diningFavorites.includes(parseInt(icon.id, 10))) {
+        console.log('Remove favorite');
+        const newFavorites = diningFavorites.filter((item) => item !== parseInt(icon.id, 10));
+        console.log('newFavorites', newFavorites);
+        setFavorites(newFavorites);
+      } else {
+        console.log('Add favorite');
+        diningFavorites.push(parseInt(icon.id, 10));
+        setFavorites(diningFavorites);
+      }
+
+      populateDiningHalls(allDiningHalls);
+    });
   });
 }
 
 async function windowActions() {
   // Create table of dining halls, set favorites.
-  //localStorage.removeItem('diningFavorites');
-  // setFavorites(['empty']);
   const allDiningHalls = await getDiningHallData();
   const favorites = getFavorites();
 
   const {diningFavorites} = favorites;
   const favoritesArr = (diningFavorites === null) ? ['empty'] : diningFavorites;
 
-  populateDiningHalls(allDiningHalls, favoritesArr);
-
-  const buttons = document.querySelectorAll('.favorite');
-  await favoritesHandler(buttons);
+  await populateDiningHalls(allDiningHalls, favoritesArr);
 }
 window.onload = windowActions;
